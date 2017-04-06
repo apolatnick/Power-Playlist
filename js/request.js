d1a9c409f3966ceac60f96ced7a391451272715a
apolatnick <apolatnick@scu.edu>
2017-04-05 21:01:14 -0700

    Update request.js

--- js/request.js
+++ js/request.js
@@ -1,8 +1,8 @@
 $(document).ready(function searchSong(){
 	$("#search").keypress(function(event){
 		if(event.which == 13){
-    	var aResult;
-			jQuery.ajax({
+    		var aResult;
+		jQuery.ajax({
     		type: "POST",
     		url: 'http://localhost:8888/~apolatnick/SeniorDesign/php/search.php',
     		dataType: 'json',
@@ -11,27 +11,31 @@ $(document).ready(function searchSong(){
 
     		success: function (result,textstatus) {
                   		if(!('error' in result))
-											{
-                      		aResult = result.output;
-													alert(aResult[0][1]);
-                      		//alert("In success function");
+				{
+                      			aResult = result.output;
+					alert(aResult[0][1]);
+                      			//alert("In success function");
+					var str = aResult[0][0];
+                          		var newList;
+                          		newList = document.querySelector(".example");
+                          		newList.appendChild(document.createElement("li")).textContent = str;
                   		}
                   		else
-											{
-												alert(result.error);
+				{
+					alert(result.error);
                   		}
             		},
-								error: function(xhr)
-								{
-									//alert("No Search Results");
-									var response = xhr.responseText;
-									alert(response);
-									var statusMessage = xhr.status + ' ' + xhr.statusText;
-									var message  = 'Query failed, php script returned this status: ';
-									var message = message + statusMessage + ' response: ' + response;
-									alert(message);
-								}
-							});
-					}
-		});
+			error: function(xhr)
+			{
+				//alert("No Search Results");
+				var response = xhr.responseText;
+				alert(response);
+				var statusMessage = xhr.status + ' ' + xhr.statusText;
+				var message  = 'Query failed, php script returned this status: ';
+				var message = message + statusMessage + ' response: ' + response;
+				alert(message);
+			}
+			});
+		}
+	});
 });
