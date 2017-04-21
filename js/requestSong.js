f6de4ff78e52f5e66ec66a883d319c917635c4ee
apolatnick <apolatnick@scu.edu>
2017-04-21 11:18:49 -0700

    Add files via upload

--- js/requestSong.js
+++ js/requestSong.js
@@ -1,32 +1,152 @@
-var clicked = 0;
 
-function loadFunction()
-{
-	alert("hi");
-	//document.getElementById("chosenSong").addEventListener("click", testFunction);
-}
-
-function addToPlaylist(song){
-	//put song at the end of the playlist
+function addToPlaylist(song,aResult){
+	//add to html playlist
 	$("#empty").remove();
 	$("#emptyalso").remove();
 	var example = document.querySelector(".example");
 	example.appendChild(song);
 	$("li:even").css("background-color", "#333333");
 
+	var arrayResult;
+	jQuery.ajax({
+			type: "POST",
+			url: 'http://localhost:8888/~apolatnick/Power-Playlist6/master/php/playlistManager.php',
+			dataType: 'json',
+			async: false,
+			data: {functionname: 'addToPlaylist', arguments: [aResult[0]]},
+
+			success: function (result,textstatus) {
+				if(!('error' in result))
+				{
+					arrayResult = result.output;
+					alert(arrayResult[0][0]);
+				}
+				else
+				{
+					alert(result.error);
+				}
+			},
+		error: function(xhr)
+		{
+			//alert("No Search Results");
+			var response = xhr.responseText;
+			alert(response);
+			var statusMessage = xhr.status + ' ' + xhr.statusText;
+			var message  = 'Query failed, php script returned this status: ';
+			var message = message + statusMessage + ' response: ' + response;
+			alert(message);
+		}
+		});
+}
+
+function moveSong(songName1,songName2)
+{
+	jQuery.ajax({
+			type: "POST",
+			url: 'http://localhost:8888/~apolatnick/Power-Playlist6/master/php/playlistManager.php',
+			dataType: 'json',
+			async: false,
+			data: {functionname: 'moveSong', arguments: [songName1,songName2]},
+
+			success: function (result,textstatus) {
+				if(!('error' in result))
+				{
+					arrayResult = result.output;
+					alert(arrayResult[0][0]);
+				}
+				else
+				{
+					alert(result.error);
+				}
+			},
+		error: function(xhr)
+		{
+			//alert("No Search Results");
+			var response = xhr.responseText;
+			alert(response);
+			var statusMessage = xhr.status + ' ' + xhr.statusText;
+			var message  = 'Query failed, php script returned this status: ';
+			var message = message + statusMessage + ' response: ' + response;
+			alert(message);
+		}
+	});
+}
+
+function deleteSong(song,list)
+{
+	jQuery.ajax({
+			type: "POST",
+			url: 'http://localhost:8888/~apolatnick/Power-Playlist6/master/php/playlistManager.php',
+			dataType: 'json',
+			async: false,
+			data: {functionname: 'deleteSong', arguments: [song,list]},
+
+			success: function (result,textstatus) {
+				if(!('error' in result))
+				{
+					arrayResult = result.output;
+					alert(arrayResult[0][0]);
+				}
+				else
+				{
+					alert(result.error);
+				}
+			},
+		error: function(xhr)
+		{
+			//alert("No Search Results");
+			var response = xhr.responseText;
+			alert(response);
+			var statusMessage = xhr.status + ' ' + xhr.statusText;
+			var message  = 'Query failed, php script returned this status: ';
+			var message = message + statusMessage + ' response: ' + response;
+			alert(message);
+		}
+	});
+}
+
+function approveSong(song)
+{
+	jQuery.ajax({
+			type: "POST",
+			url: 'http://localhost:8888/~apolatnick/Power-Playlist6/master/php/playlistManager.php',
+			dataType: 'json',
+			async: false,
+			data: {functionname: 'approveSong', arguments: [song]},
+
+			success: function (result,textstatus) {
+				if(!('error' in result))
+				{
+					arrayResult = result.output;
+					alert(arrayResult[0][0]);
+				}
+				else
+				{
+					alert(result.error);
+				}
+			},
+		error: function(xhr)
+		{
+			//alert("No Search Results");
+			var response = xhr.responseText;
+			alert(response);
+			var statusMessage = xhr.status + ' ' + xhr.statusText;
+			var message  = 'Query failed, php script returned this status: ';
+			var message = message + statusMessage + ' response: ' + response;
+			alert(message);
+		}
+	});
 }
 
 
 $(document).ready(function searchSong(){
 	$("#find").keypress(function(event){
 		if(event.which == 13){
-			//if(clicked == 0){
-				//clicked++;
     		var aResult;
 				var newList;
 		jQuery.ajax({
     		type: "POST",
-    		url: 'http://localhost:8888/~apolatnick/Power-Playlist4/master/php/search.php',
+    		url: 'http://localhost:8888/~apolatnick/Power-Playlist6/master/php/search.php',
     		dataType: 'json',
 				async: false,
     		data: {functionname: 'find', arguments: [$("#find").val()]},
@@ -40,33 +160,19 @@ $(document).ready(function searchSong(){
           	newList = document.querySelector("#dropdown");
 						for(var i = 0; i < aLength; i++)
 						{
-							//concatinate song name with artist maybe? make it a variable and use it for text content?
 							var obj;
-							//obj = newList.appendChild(document.createElement("li"));
 							var txt = aResult[i][0] + " - " + aResult[i][1];
 							$(newList).append('<li class="searchList" data-ogg='+aResult[i][5]+'>'+aResult[i][0]+'<br /><p class="artist">'+aResult[i][1]+'</p></li>');
-							//obj.textContent = txt;
-							//$(obj).attr("data-ogg",aResult[i][5]);
-							//$(obj).addClass("searchList");
 							$(".searchList").click(function(e){
-								//clicked = 0;
-								addToPlaylist(e.target);
-								//newList.remove();
+								addToPlaylist(e.target,aResult);
+								newList.empty();
 							});
 
 						}
-						// newList.addEventListener("cleck",function(e)){
-						// 	if(e.target && e.target.nodeName === "LI")
-						// 	{
-						// 		alert("hi");
-						// 	}
-						// });
 						if(aLength == 0)
 						{
 							newList.appendChild(document.createElement("li")).textContent = "no results found";
 						}
-						//var nextSong = document.getElementById("#dropdown li");
-						//nextSong.addEventListener("onclick", function(){alert("success");});
         	}
         	else
 					{
