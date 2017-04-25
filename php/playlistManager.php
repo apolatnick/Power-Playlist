65892e7fa4010f7dc56cedba833175956f7d8d74
apolatnick <apolatnick@scu.edu>
2017-04-25 16:55:25 -0700

    Add files via upload

--- php/playlistManager.php
+++ php/playlistManager.php
@@ -1,9 +1,26 @@
 <?php
-$a = ["a","ap",1];
-$b = ["b","alex",2];
-$c = ["c","alexander",3];
-$d = ["d","alepol",4];
-$e = ["e","apo",5];
+// $a = ["a","ap",1];
+// $b = ["b","alex",2];
+// $c = ["c","alexander",3];
+// $d = ["d","alepol",4];
+// $e = ["e","apo",5];
+
+function updatePlaylists($list){
+  if($list == 1){
+    if(filesize("../txt/playlist.txt") != 0)
+    {
+      $playlist = unserialize(file_get_contents("../txt/playlist.txt"));
+    }
+    return array_values($playlist);
+  }
+  else{
+    if(filesize("../txt/suggestedPlaylist.txt") != 0)
+    {
+      $playlist = unserialize(file_get_contents("../txt/suggestedPlaylist.txt"));
+    }
+    return array_values($playlist);
+  }
+}
 
 function addToPlaylist($input){
 //read and unserialize file contents
@@ -65,11 +82,16 @@ function deleteSong($input,$list){
         }
       }
       unset($playlist[count($playlist)-1]);
+
       $playlistPHP = $playlist;
       $myfile = fopen("../txt/playlist.txt", "w") or die("Unable to open file!");
       fwrite($myfile, serialize($playlist));
       fclose($myfile);
       //echo $playlistPHP[1][0];
+      //return array_values($playlistPHP);
+      //file_put_contents("../txt/t.txt",array_values($playlistPHP[0]));
+      //file_put_contents("scratch2.txt", json_encode(array_values($playlistPHP)));
+
       return array_values($playlistPHP);
     }
   }
@@ -236,94 +258,111 @@ function approveSong($input){
 //deleteSong("d",1);
 //upVote("a",1);
 //approveSong("d");
-moveSong("a","c");
+//moveSong("a","c");
 
 
 
-// $result = array();
-// if( !isset($_POST['functionname']) ){
-//     $result['error'] = 'No function name';
-//     // file_put_contents("txt/t.txt","No function name");
-//  }
-// elseif( !isset($_POST['arguments']) ) {
-//     $result['error'] = 'No function arguments';
-//     // file_put_contents("txt/t.txt","No function arguments!");
-//  }
-// else{
-// switch($_POST['functionname']) {
-//         case 'addToPlaylist':
-//            if( !is_array($_POST['arguments']) || (count($_POST['arguments']) < 1) ) {
-//                $result['error'] = 'Error in function arguments';
-//               //  file_put_contents("txt/t.txt","Error in function arguments!");
-//            }
-//            else {
-//               $result['output'] = addToPlaylist($_POST['arguments'][0]);
-//               // file_put_contents("txt/t.txt",$_POST['functionname']);
-//            }
-//            break;
-//         case 'addToSuggestedPlaylist':
-//             if( !is_array($_POST['arguments']) || (count($_POST['arguments']) < 1) ) {
-//                 $result['error'] = 'Error in function arguments';
-//                 //  file_put_contents("txt/t.txt","Error in function arguments!");
-//             }
-//             else {
-//                 $result['output'] = addToSuggestedPlaylist($_POST['arguments'][0]);
-//                 // file_put_contents("txt/t.txt",$_POST['functionname']);
-//             }
-//         case 'deleteSong':
-//             if( !is_array($_POST['arguments']) || (count($_POST['arguments']) < 2) ) {
-//                 $result['error'] = 'Error in function arguments';
-//                 //  file_put_contents("txt/t.txt","Error in function arguments!");
-//             }
-//             else {
-//                 $result['output'] = deleteSong($_POST['arguments'][0],$_POST['arguments'][1]);
-//                 // file_put_contents("txt/t.txt",$_POST['functionname']);
-//             }
-//         case 'moveSong':
-//             if( !is_array($_POST['arguments']) || (count($_POST['arguments']) < 2) ) {
-//                 $result['error'] = 'Error in function arguments';
-//                 //  file_put_contents("txt/t.txt","Error in function arguments!");
-//             }
-//             else {
-//                 $result['output'] = moveSong($_POST['arguments'][0],$_POST['arguments'][1]);
-//                 // file_put_contents("txt/t.txt",$_POST['functionname']);
-//             }
-//         case 'upVote':
-//             if( !is_array($_POST['arguments']) || (count($_POST['arguments']) < 2) ) {
-//                 $result['error'] = 'Error in function arguments';
-//                 //  file_put_contents("txt/t.txt","Error in function arguments!");
-//             }
-//             else {
-//                 $result['output'] = upVote($_POST['arguments'][0],$_POST['arguments'][1]);
-//                 // file_put_contents("txt/t.txt",$_POST['functionname']);
-//             }
-//         case 'downVote':
-//             if( !is_array($_POST['arguments']) || (count($_POST['arguments']) < 2) ) {
-//                 $result['error'] = 'Error in function arguments';
-//                 //  file_put_contents("txt/t.txt","Error in function arguments!");
-//             }
-//             else {
-//                 $result['output'] = downVote($_POST['arguments'][0],$_POST['arguments'][1]);
-//                 // file_put_contents("txt/t.txt",$_POST['functionname']);
-//             }
-//         case 'approveSong':
-//             if( !is_array($_POST['arguments']) || (count($_POST['arguments']) < 1) ) {
-//                 $result['error'] = 'Error in function arguments';
-//                 //  file_put_contents("txt/t.txt","Error in function arguments!");
-//             }
-//             else {
-//                 $result['output'] = approveSong($_POST['arguments'][0]);
-//                 // file_put_contents("txt/t.txt",$_POST['functionname']);
-//             }
-//             break;
-//
-//         default:
-//           //  file_put_contents("txt/t.txt","Could not match function!");
-//            $result['error'] = 'Could not match function';
-//            break;
-//       }
-//   }
-//   //show i use a json encoded array? wasnt working for me before...
-//   echo json_encode($result);
+$result = array();
+if( !isset($_POST['functionname']) ){
+    $result['error'] = 'No function name';
+    // file_put_contents("txt/t.txt","No function name");
+ }
+elseif( !isset($_POST['arguments']) ) {
+    $result['error'] = 'No function arguments';
+    // file_put_contents("txt/t.txt","No function arguments!");
+ }
+else{
+switch($_POST['functionname']) {
+        case 'updatePlaylists':
+          if( !is_array($_POST['arguments']) || (count($_POST['arguments']) < 1) ) {
+              $result['error'] = 'Error in function arguments';
+              //  file_put_contents("txt/t.txt","Error in function arguments!");
+            }
+            else {
+              $result['output'] = updatePlaylists($_POST['arguments'][0]);
+              // file_put_contents("txt/t.txt",$_POST['functionname']);
+            }
+            break;
+        case 'addToPlaylist':
+           if( !is_array($_POST['arguments']) || (count($_POST['arguments']) < 1) ) {
+               $result['error'] = 'Error in function arguments';
+              //  file_put_contents("txt/t.txt","Error in function arguments!");
+           }
+           else {
+              $result['output'] = addToPlaylist($_POST['arguments'][0]);
+              // file_put_contents("txt/t.txt",$_POST['functionname']);
+           }
+           break;
+        case 'addToSuggestedPlaylist':
+            if( !is_array($_POST['arguments']) || (count($_POST['arguments']) < 1) ) {
+                $result['error'] = 'Error in function arguments';
+                //  file_put_contents("txt/t.txt","Error in function arguments!");
+            }
+            else {
+                $result['output'] = addToSuggestedPlaylist($_POST['arguments'][0]);
+                // file_put_contents("txt/t.txt",$_POST['functionname']);
+            }
+            break;
+        case 'deleteSong':
+            if( !is_array($_POST['arguments']) || (count($_POST['arguments']) < 2) ) {
+                $result['error'] = 'Error in function arguments';
+                //  file_put_contents("txt/t.txt","Error in function arguments!");
+            }
+            else {
+                $result['output'] = deleteSong($_POST['arguments'][0],$_POST['arguments'][1]);
+                //file_put_contents("scratch3.txt", json_encode($result));
+                //file_put_contents("../txt/t.txt",$result['output']);
+                // file_put_contents("txt/t.txt",$_POST['functionname']);
+            }
+            break;
+        case 'moveSong':
+            if( !is_array($_POST['arguments']) || (count($_POST['arguments']) < 2) ) {
+                $result['error'] = 'Error in function arguments';
+                //  file_put_contents("txt/t.txt","Error in function arguments!");
+            }
+            else {
+                $result['output'] = moveSong($_POST['arguments'][0],$_POST['arguments'][1]);
+                // file_put_contents("txt/t.txt",$_POST['functionname']);
+            }
+            break;
+        case 'upVote':
+            if( !is_array($_POST['arguments']) || (count($_POST['arguments']) < 2) ) {
+                $result['error'] = 'Error in function arguments';
+                //  file_put_contents("txt/t.txt","Error in function arguments!");
+            }
+            else {
+                $result['output'] = upVote($_POST['arguments'][0],$_POST['arguments'][1]);
+                // file_put_contents("txt/t.txt",$_POST['functionname']);
+            }
+            break;
+        case 'downVote':
+            if( !is_array($_POST['arguments']) || (count($_POST['arguments']) < 2) ) {
+                $result['error'] = 'Error in function arguments';
+                //  file_put_contents("txt/t.txt","Error in function arguments!");
+            }
+            else {
+                $result['output'] = downVote($_POST['arguments'][0],$_POST['arguments'][1]);
+                // file_put_contents("txt/t.txt",$_POST['functionname']);
+            }
+            break;
+        case 'approveSong':
+            if( !is_array($_POST['arguments']) || (count($_POST['arguments']) < 1) ) {
+                $result['error'] = 'Error in function arguments';
+                //  file_put_contents("txt/t.txt","Error in function arguments!");
+            }
+            else {
+                $result['output'] = approveSong($_POST['arguments'][0]);
+                // file_put_contents("txt/t.txt",$_POST['functionname']);
+            }
+            break;
+
+        default:
+          //  file_put_contents("txt/t.txt","Could not match function!");
+           $result['error'] = 'Could not match function';
+           break;
+      }
+  }
+  //file_put_contents("scratch.txt", json_encode($result));
+  echo json_encode($result);
 
 ?>
